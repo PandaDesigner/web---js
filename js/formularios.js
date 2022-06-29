bcc509aae950289d988d3c31a9296a9e8c8759b4
Pedro Fernandez <pedro@pandadesigners.com>
2022-06-28 23:50:19 -0500

    main

---
+++ js/formularios.js
@@ -0,0 +1,64 @@
+
+// propiedades formulario
+const d = document;
+const propFormulario = {
+    formulario: d.formulario_contacto,
+    formElementos: d.formulario_contacto.elements,
+    element: [],
+    focusElements: null,
+    blurElements: null,
+    error: null,
+    textoError: null
+}
+const { formulario, formElementos, element, focusElements,blurElements,error, textoError } = propFormulario;
+
+// metdos del formulario
+
+const methFormulario ={
+    inicio: function () {
+        for (const index in propFormulario.formElementos) {
+           if(propFormulario.formElementos[index].type == 'text' || propFormulario.formElementos[index].type == 'email' || propFormulario.formElementos[index].type == 'textarea'){
+            propFormulario.formElementos[index].addEventListener('focus', focusInput)
+            propFormulario.formElementos[index].addEventListener('blur', blurInput)
+
+        }}
+        formulario.addEventListener('submit', validarInput)
+    },
+    focusInput: function(){
+
+        this.parentElement.children[1].className = 'label active';
+    },
+    blurInput: function(){
+
+        (this.value != '')? this.parentElement.children[1].className = 'label active': this.parentElement.children[1].className = 'label';
+    },
+
+    validarInput: (e) =>{
+        for (const index in propFormulario.formElementos) {
+            if(propFormulario.formElementos[index].value == ''){
+
+                e.preventDefault();
+
+                if(propFormulario.formElementos[index].parentElement.children.length < 3 ){
+
+                propFormulario.error = d.createElement('p');
+                propFormulario.textoError = d.createTextNode(`Por favor llena el campo con tu ${propFormulario.formElementos[index].name}`);
+                propFormulario.error.appendChild(propFormulario.textoError);
+                propFormulario.error.className = 'error';
+                propFormulario.formElementos[index].parentElement.appendChild(propFormulario.error);
+                }
+
+
+            }
+      else{
+              if(formElementos[index].parentElement.children.length >= 3){
+                  propFormulario.error = formElementos[index].parentElement.getElementsByTagName('p')[0];
+                  formElementos[index].parentElement.removeChild(propFormulario.error);
+              }
+      }}
+  }
+};
+
+const {inicio, focusInput, blurInput, validarInput} = methFormulario;
+
+methFormulario.inicio();
--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,12 @@
 const propScroll={
-    positionScroll: window.pageYOffset,
+    posicion: window.pageYOffset,
     scrollSuave: document.getElementsByClassName('scroll-suave'),
     returTop: document.getElementsByClassName('retur-top'),
+    destino: null,
+    seccionDestino: null,
+    intervalo: null,
 }
-const { positionScroll, scrollSuave, returTop } = propScroll;
+
 
 
 const metScroll = {
@@ -17,21 +20,42 @@ const metScroll = {
         for (const j in returTop ) {
 
                 returTop[j].addEventListener('click', subir );
+
         }
 
 
     },
     moverse: function (e) {
-        e.preventDefault()
-        console.log(this.innerHTML);
+        e.preventDefault();
+        propScroll.destino = this.getAttribute('href');
+        propScroll.seccionDestino = document.querySelector( propScroll.destino).offsetTop;
+        propScroll.posicion = window.pageYOffset;
+        propScroll.intervalo = setInterval(function (){
+
+            if(propScroll.posicion < propScroll.seccionDestino ){
+
+                propScroll.posicion += 1;
+
+                if(popScroll.posicion >= propScroll.seccionDestino){
+                    clearInterval(propScroll.intervalo);
+                }
+            }
+
+            window.scrollTo(0, propScroll.posicion);
+
+        }, 15);
+
+
+        console.log(propScroll.seccionDestino, propScroll.destino);
 
     },
+
     subir: function (e) {
-        e.preventDefault()
-        return console.log(this.innerHTML);
+        e.preventDefault();
+
     }
 }
-
+const { posicion, scrollSuave, returTop, destino } = propScroll;
 const { inicio, moverse, subir }= metScroll;
 
 inicio()
